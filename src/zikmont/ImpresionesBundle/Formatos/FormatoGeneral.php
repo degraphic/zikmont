e3cfe1de5900cca31e5b3115eb7483064c5a3d7f
zikmont <zikmont@zikmont>
2013-04-09 00:39:07 -0500

    formato

--- src/zikmont/ImpresionesBundle/Formatos/FormatoGeneral.php
+++ src/zikmont/ImpresionesBundle/Formatos/FormatoGeneral.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace zikmont\ImpresionesBundle\Formatos;
+
 use zikmont\ImpresionesBundle\Formatos\EncabezadoFormatosOperativos;
+
 //require_once '/opt/lampp/htdocs/zikmont/src/zikmont/ImpresionesBundle/Reportes/fpdf.php';
 require_once __DIR__ . '/../Soporte/fpdf.php';
 
@@ -11,7 +13,28 @@ require_once __DIR__ . '/../Soporte/fpdf.php';
  * @author daniel
  */
 class FormatoGeneral extends \FPDF {
-    
+
+    public function GenerarEncabezado($em) {
+
+        $arEmpresa = new \zikmont\FrontEndBundle\Entity\GenEmpresas;
+        $arEmpresa = $em->getRepository('zikmontFrontEndBundle:GenEmpresas')->find(1);
+//      $arReporte = new \zikmont\ImpresionesBundle\Entity\ImpReportes();
+//      $arReporte = $em->getRepository('zikmontImpresionesBundle:ImpReportes')->find($intCodigoReporte);    
+
+        $this->SetFont('Arial', '', 4);
+        $this->Text(186, 2, '2013-01-01 10:25 [Zikmont - Inventario]');
+        $this->Image('bundles/zikmont/imagenes/logos/abac.jpg', 4, 2, 33);
+        $this->ln(11);        $this->Text(160, 2, '2013-01-01 10:25 [Zikmont Contable]');
+
+        $this->SetFont('Arial', 'B', 12);
+//        $this->Text(70, 15, self::$arMovimiento->getDocumentoRel()->getNombre());
+        $this->ln(5);
+        $this->SetFont('Arial', 'B', 8);
+//        $this->Text(4, 22, $arEmpresa->getNombreEmpresa(). "-" . $arEmpresa->getNit() . "-" . $arEmpresa->getDigitoVerificacion() );
+//        $this->SetFont('Arial', '', 8);
+        $this->Text(10,22, $arEmpresa->getNit() . "-" . $arEmpresa->getDigitoVerificacion() );
+        $this->SetXY(258, 18);
+    }
 }
 
 ?>
--- src/zikmont/ImpresionesBundle/Formatos/FormatoSolicitud.php
+++ src/zikmont/ImpresionesBundle/Formatos/FormatoSolicitud.php
@@ -1,89 +1,103 @@
 <?php
+
 namespace zikmont\ImpresionesBundle\Formatos;
 
 class FormatoSolicitud extends FormatoGeneral {
-    
-    public $Pdf;
-    
-    public function Generar($em, $arMovimiento) {        
-        ob_end_clean();        
-        $this->Pdf = new FormatoSolicitud('P', 'mm', 'letter', $em);           
-        EncabezadoFormatosOperativos::GenerarEncabezado($em, 3,$this->Pdf);        
 
-        $this->Pdf->AddPage();      
-        
-        $this->Pdf->SetMargins(4, 1, 10);
-        $this->Pdf->Rect(4, 25, 207, 18);
+    public static $arMovimiento;
+    public static $em;
 
-        $this->Pdf->SetFont('Arial', 'B', 7);
-        // Listado derecho.
-        $this->Pdf->SetY(26);
-        $List1 = array('TERCERO: ', 'NIT: ', 'DIR:', 'CIUDAD:');
+    public function Generar($em, $arMovimiento) {
+        ob_end_clean();
+        $this->Pdf = new FormatoSolicitud('P', 'mm', 'letter', $em);
+        self::$arMovimiento = $arMovimiento;
+        self::$em = $em;
+        $this->Pdf->AddPage();
+        $this->Pdf->Output("Solicitud".$arMovimiento->getNumeroMovimiento.".pdf", 'D');
+        exit;
+    }
+
+    public function Header() {
+        $this->GenerarEncabezado(self::$em);
+
+        $this->SetMargins(4, 1, 10);
+        $this->Rect(4, 25, 207, 18);
+        $this->ln(1);
+        $this->SetY(26);
+        $this->SetFont('Arial', 'B', 7);
+        $List1 = array('CLIENTE: ', 'NIT: ', 'DIR:', 'CIUDAD:');
         foreach ($List1 as $col) {
-            $this->Pdf->Cell(40, 5, $col, 0, 0, 'L');
-            $this->Pdf->Ln(3.5);
+            $this->Cell(40, 5, $col, 0, 0, 'L');
+            $this->Ln(3.5);
         }
         
-        $Datos = array($arMovimiento->getTerceroRel()->getNombreCortoTercero(), $arMovimiento->getTerceroRel()->getCodigoTerceroPk(), $arMovimiento->getTerceroRel()->getTelefono(), $arMovimiento->getTerceroRel()->getDireccion());
-        $this->Pdf->SetFont('Arial', '', 8);
-        $this->Pdf->SetY(26);
+        $Datos = array(self::$arMovimiento->getTerceroRel()->getNombreCortoTercero(), self::$arMovimiento->getTerceroRel()->getCodigoTerceroPk(), self::$arMovimiento->getTerceroRel()->getTelefono(),self::$arMovimiento->getTerceroRel()->getDireccion());
+        $this->SetFont('Arial', '', 8);
+        $this->SetY(26);
 
         foreach ($Datos as $col) {
-            $this->Pdf->SetX(28);
-            $this->Pdf->Cell(40, 5, $col, 0, 'R');
-            $this->Pdf->Ln(3.5);
+            $this->SetX(28);
+            $this->Cell(40, 5, $col, 0, 'R');
+            $this->Ln(3.5);
         }
-        
-        
-        $this->Pdf->SetXY(4,45);        
-        $this->Pdf->SetFillColor(236, 236, 236);
-        $this->Pdf->SetTextColor(0);
-        $this->Pdf->SetDrawColor(0, 0, 0);
-        $this->Pdf->SetLineWidth(.3);
-        $this->Pdf->SetFont('', 'B', 6.3);
+
+        $this->Body();
+    }
+
+    public function Body() {
+        $this->SetFont('Arial', '', 7);
+        $this->SetXY(4, 45);
+        $this->SetFillColor(236, 236, 236);
+        $this->SetTextColor(0);
+        $this->SetDrawColor(0, 0, 0);
+        $this->SetLineWidth(.3);
+        $this->SetFont('', 'B', 6.3);
         
         $arrayTitulos = array('ITEM', 'DESCRIPCION', 'LOTE', 'VENCIMIENTO', 'BODEGA', 'COSTO', 'IVA', 'CANTIDAD', 'TOTAL');
         $arrayMedidas = array(9, 96, 20, 16, 11, 14, 8, 13, 20);
+        
         for ($i = 0; $i < count($arrayTitulos); $i++)
-            $this->Pdf->Cell($arrayMedidas[$i], 4, $arrayTitulos[$i], 1, 0, 'C', 1);
-       
-        $this->Pdf->SetFillColor(224, 235, 255);
-        $this->Pdf->SetTextColor(0);
-        $this->Pdf->SetFont('');
-        $this->Pdf->Ln(1);
-        
+            $this->Cell($arrayMedidas[$i], 4, $arrayTitulos[$i], 1, 0, 'C', 1);
+
+        $this->ln(5);
+
+        $this->SetFillColor(224, 235, 255);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        $this->Ln(1);
+
         $arMovimientosDetalle = new \zikmont\InventarioBundle\Entity\InvMovimientosDetalles();
-        $arMovimientosDetalle = $em->getRepository('zikmontInventarioBundle:InvMovimientosDetalles')->findBy(array('codigoMovimientoFk' => $arMovimiento->getCodigoMovimientoPk()));
-        $this->Pdf->SetFont('Arial', '', 6);
-        $this->Pdf->SetXY(4,48);
-        foreach($arMovimientosDetalle as $Detalle) {
-            $this->Pdf->SetFont('Arial', '', 6.5);
+        $arMovimientosDetalle = self::$em->getRepository('zikmontInventarioBundle:InvMovimientosDetalles')->findBy(array('codigoMovimientoFk' => self::$arMovimiento->getCodigoMovimientoPk()));
+        $this->SetFont('Arial', '', 6);
+        $this->SetXY(4, 48);
+        foreach ($arMovimientosDetalle as $Detalle) {
+            $this->SetFont('Arial', '', 6.5);
 
-            $this->Pdf->Cell(9, 5, $Detalle->getMovimientoRel()->getCodigoMovimientoPk(), 0, 0, 'L');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(96, 5, $Detalle->getitemRel()->getDescripcion(), 0, 0, 'L');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(20, 5, $Detalle->getLoteFk(), 0, 0, 'L');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(16, 5, $Detalle->getCodigoMovimientoFk(), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(11, 5, $Detalle->getCodigoBodegaFk(), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(14, 5, number_format($Detalle->getPrecio(), 2, '.', ','), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(8, 5, number_format($Detalle->getPorcentajeIva(), 0, '.', ','), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(13, 5, number_format($Detalle->getCantidad(), 0, '.', ','), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
-            $this->Pdf->Cell(20, 5, number_format($Detalle->getTotal(), 2, '.', ','), 0, 0, 'R');
-            $this->Pdf->line($this->Pdf->GetX(), $this->Pdf->GetY() + 4, $this->Pdf->GetX(), $this->Pdf->GetY() + 1);
+            $this->Cell(9, 5, $Detalle->getMovimientoRel()->getCodigoMovimientoPk(), 0, 0, 'L');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(96, 5, $Detalle->getitemRel()->getDescripcion(), 0, 0, 'L');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(20, 5, $Detalle->getLoteFk(), 0, 0, 'L');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(16, 5, $Detalle->getCodigoMovimientoFk(), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(11, 5, $Detalle->getCodigoBodegaFk(), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(14, 5, number_format($Detalle->getPrecio(), 2, '.', ','), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(8, 5, number_format($Detalle->getPorcentajeIva(), 0, '.', ','), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(13, 5, number_format($Detalle->getCantidad(), 0, '.', ','), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
+            $this->Cell(20, 5, number_format($Detalle->getTotal(), 2, '.', ','), 0, 0, 'R');
+            $this->line($this->GetX(), $this->GetY() + 4, $this->GetX(), $this->GetY() + 1);
 
-            $this->Pdf->Ln(4);
+            $this->Ln(4);
         }
-        
-        $this->Pdf->SetFont('Arial', 'B', 7.5);
-        $this->Pdf->line(6, $this->Pdf->GetY() + 10, 211, $this->Pdf->GetY() + 10);
-        $this->Pdf->ln(12);
+
+        $this->SetFont('Arial', 'B', 7.5);
+        $this->line(6, $this->GetY() + 10, 211, $this->GetY() + 10);
+        $this->ln(12);
         $totales = array('SUBTOTAL: ' . " " . " ",
             'DCTO: ' . " " . " ",
             'SUBTOTAL NETO: ' . " " . " ",
@@ -92,43 +106,50 @@ class FormatoSolicitud extends FormatoGeneral {
             'TOTAL GENERAL: ' . " " . " ",
             'RTE FTE: ' . " " . " ");
 
-        $this->Pdf->line(6, $this->Pdf->GetY() + 30, 211, $this->Pdf->GetY() + 30);
-        $this->Pdf->SetMargins(170, 2, 15);
+        $this->line(6, $this->GetY() + 30, 211, $this->GetY() + 30);
+        $this->SetMargins(170, 2, 15);
         for ($i = 0; $i < count($totales); $i++) {
-            $this->Pdf->SetX(170);
-            $this->Pdf->Cell(20, 4, $totales[$i], 0, 0, 'R');
-            $this->Pdf->ln();
+            $this->SetX(170);
+            $this->Cell(20, 4, $totales[$i], 0, 0, 'R');
+            $this->ln();
         }
 
-        $totales2 = array(number_format($arMovimiento->getSubTotal(), 2, '.', ','),
-            number_format($arMovimiento->getValorTotalDescuento(), 2, '.', ','),
-            number_format($arMovimiento->getSubTotal(), 2, '.', ','),
-            number_format($arMovimiento->getSubTotal(), 2, '.', ','),
-            number_format($arMovimiento->getSubTotal(), 2, '.', ','),
-            number_format($arMovimiento->getSubTotal(), 2, '.', ','),
-            number_format($arMovimiento->getSubTotal(), 2, '.', ','));
+        $totales2 = array(number_format(self::$arMovimiento->getSubTotal(), 2, '.', ','),
+            number_format(self::$arMovimiento->getValorTotalDescuento(), 2, '.', ','),
+            number_format((self::$arMovimiento->getSubTotal() - self::$arMovimiento->getValorTotalDescuento()), 2, '.', ','),
+            number_format(self::$arMovimiento->getValorTotalIva(), 2, '.', ','),
+            number_format(self::$arMovimiento->getSubTotal(), 2, '.', ','),
+            number_format(self::$arMovimiento->getSubTotal(), 2, '.', ','),
+            number_format(self::$arMovimiento->getSubTotal(), 2, '.', ','));
 
-        $this->Pdf->SetFont('Arial', '', 7.5);
-        $this->Pdf->SetXY(193, $this->Pdf->GetY() - 40);
-        $this->Pdf->ln(12);
+        $this->SetFont('Arial', '', 7.5);
+        $this->SetXY(193, $this->GetY() - 40);
+        $this->ln(12);
         for ($i = 0; $i < count($totales2); $i++) {
-            $this->Pdf->SetX(192);
-            $this->Pdf->Cell(20, 4, $totales2[$i], 0, 0, 'R');
-            $this->Pdf->ln();
+            $this->SetX(192);
+            $this->Cell(20, 4, $totales2[$i], 0, 0, 'R');
+            $this->ln();
         }
+
+        $this->SetFont('Arial', 'B', 6);
         
-        $this->Pdf->SetFont('Arial', 'B', 6);
-        
-        //$this->Pdf->Text(22, 20, "Preparado");
-        $this->Pdf->Text(130, $this->Pdf->getY()+20, "AUTORIZADO");
-        
-        $this->Pdf->Text(170, $this->Pdf->getY()+20, "FIRMA Y SELLO RECIBO");
-        $this->Pdf->Text(190, $this->Pdf->getY()+25, "C.C / Nit");                        
-        
-        $this->Pdf->Output($arMovimiento->getDocumentoRel()->getNombre().".pdf", 'D');
+        $this->Cell(0, 0, $this->line(100, $this->GetY($this->SetY(130)), 150, $this->GetY($this->SetY(130))) . $this->Text(100, $this->GetY($this->SetY(133)), "AUTORIZADO"));
+        $this->Cell(0, 0, $this->line(160, $this->GetY($this->SetY(130)), 210, $this->GetY($this->SetY(130))) . $this->Text(160, $this->GetY($this->SetY(133)), "FIRMADO RECIBIDO"));
+
+        $this->Output(self::$arMovimiento->getDocumentoRel()->getNombre() . ".pdf", 'D');
         exit;
-    }           
+    }
+
+    public function Footer() {
+        $this->SetFont('Arial', '', 8);
+        $this->Rect(9, 265, 70, 6);
+        $this->Text(14, 269, 'Consecutivo' . " " . $this->Rect(31, 266, 4, 4));
+        $this->Text(38, 269, 'Control');
+        $this->Rect(48, 266, 4, 4);
+        $this->Text(56, 269, 'Contabilidad' . " " . $this->Rect(73, 266, 4, 4));
+
+        //Número de página
+        $this->Text(9, 260, 'Page ' . $this->PageNo() . '/{nb}');
+    }
 }
-
 ?>
-
